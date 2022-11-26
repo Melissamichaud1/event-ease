17937bed390e6d729f83868dbbb6ce0465c2c2e3
Melissa <melissamichaud0319@gmail.com>
2022-11-26 15:52:30 -0500

    New conference form

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,68 +1,74 @@
 function createCard(title, description, pictureUrl, startDate, endDate, location) {
-    return `
-        <div class="col-sm-6 col-md-4 mb-1">
-            <div class="shadow-lg p-3 mb-5 bg-body rounded">
-                <img src="${pictureUrl}" class="card-img-top">
-                <div class="card-body">
-                    <h5 class="card-title">${title}</h5>
-                    <h6 class="card-subtitle text-muted">${location}</h6>
-                    <p class="card-text">${description}</p>
-                </div>
-                <div class="card-footer">${startDate} - ${endDate}</div>
-            </div>
-        </div>
-        `;
-    }
+  return `
+      <div class="col-sm-6 col-md-4 mb-1">
+          <div class="card shadow-lg p-3 mb-5 bg-body rounded">
+              <img src="${pictureUrl}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">${title}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">${location}</h5>
+                  <p class="card-text">${description}</p>
+              </div>
+              <div class="card-footer">
+                  ${startDate} - ${endDate}
+              </div>
+          </div>
+      </div>
+      `;
+  }
 
 function errorAlert(e) {
-    return `
-    <div class="alert alert-danger" role="alert">
-        ${e}
-    </div>
-    `;
+  return `
+      <div class="alert alert-danger alert-dismissible fade show" role="alert">
+          ${e}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              <span aria-hidden="true">&times;</span>
+      </div>
+  `;
 }
 
-  window.addEventListener('DOMContentLoaded', async () => {
 
-    const url = 'http://localhost:8000/api/conferences/';
+window.addEventListener('DOMContentLoaded', async () => {
 
-    try {
+  const url = 'http://localhost:8000/api/conferences/';
+
+  try {
       const response = await fetch(url);
 
       if (!response.ok) {
-        // Figure out what to do when the response is bad
-        let e = "There is an error!";
-        const html = errorAlert(e);
-        const error = document.querySelector('.row');
-        error.innerHTML = html;
-
+          // Figure out what to do when the response is bad
+          const e = "There has been an error!"
+          console.error(e)
+          const html = errorAlert(e);
+          const column = document.querySelector('.row');
+          column.innerHTML += html;
       } else {
-        const data = await response.json();
+          const data = await response.json();
 
-        for (let conference of data.conferences) {
-          const detailUrl = `http://localhost:8000${conference.href}`;
-          const detailResponse = await fetch(detailUrl);
-          if (detailResponse.ok) {
-            const details = await detailResponse.json();
-            const name = details.conference.name;
-            const description = details.conference.description;
-            const pictureUrl = details.conference.location.picture_url;
-            const startDate = new Date(details.conference.starts).toLocaleDateString();
-            const endDate = new Date(details.conference.ends).toLocaleDateString();
-            const location = details.conference.location.name;
-            const html = createCard(name, description, pictureUrl, startDate, endDate, location);
-            const column = document.querySelector('.row');
-            column.innerHTML += html;
+          for (let conference of data.conferences) {
+              const detailUrl = `http://localhost:8000${conference.href}`;
+              const detailResponse = await fetch(detailUrl);
+              if (detailResponse.ok) {
+                  const details = await detailResponse.json();
+                  const title = details.conference.name;
+                  const description = details.conference.description;
+                  const pictureUrl = details.conference.location.picture_url;
+                  const startDate = new Date(details.conference.starts);
+                  const start = `${startDate.getMonth()}/${startDate.getDate()}/${startDate.getFullYear()}`
+                  const endDate = new Date(details.conference.ends);
+                  const end = `${endDate.getMonth()}/${endDate.getDate()}/${endDate.getFullYear()}`
+                  const location = details.conference.location.name;
+                  const html = createCard(title, description, pictureUrl, start, end, location);
+                  const column = document.querySelector('.row');
+                  column.innerHTML += html;
+              }
           }
-        }
-        }
-    } catch (e) {
+      }
+  } catch (e) {
       // Figure out what to do if an error is raised
-      console.error(e);
-      const html = errorAlert("This error occurred in the host!");
+      console.error(e)
+      const html = errorAlert(e);
       const error = document.querySelector('.row');
-      error.innerHTML = html;
-    }
+      error.innerHTML += html;
+  }
 
-
-});
+  });
--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -1,38 +1,45 @@
 window.addEventListener('DOMContentLoaded', async () => {
 
-    const url = 'http://localhost:8000/api/states/';
+  const url = 'http://localhost:8000/api/states/'
 
-    const response = await fetch(url);
+  try {
+      const response = await fetch(url);
 
-    if (response.ok) {
-      const data = await response.json();
+      if (!response.ok) {
+          console.error("there has been an error")
+      } else {
+          const data = await response.json();
+          const selectTag = document.getElementById('state');
+          console.log(data.states)
 
-      const element = document.getElementById('state');
-      for (let state of data.states) {
-          let option = document.createElement('option');
-          option.value = state.abbreviation;
-          option.innerHTML = state.name;
-          element.appendChild(option);
+          for (let state of data.states) {
+              let option = document.createElement('option');
+              option.value = Object.values(state);
+              option.innerHTML = Object.keys(state);
+              selectTag.appendChild(option);
           }
+
+          const formTag = document.getElementById('create-location-form');
+          formTag.addEventListener('submit', async event => {
+              event.preventDefault();
+              const formData = new FormData(formTag);
+              const json = JSON.stringify(Object.fromEntries(formData));
+              const locationUrl = 'http://localhost:8000/api/locations/';
+              const fetchConfig = {
+                  method: "post",
+                  body: json,
+                  headers: {
+                      'Content-Type': 'application/json',
+                  },
+              };
+              const response = await fetch(locationUrl, fetchConfig);
+              if (response.ok) {
+                  formTag.reset();
+                  const newLocation = await response.json()
+              }
+          });
       }
-      const formTag = document.getElementById('create-location-form');
-      formTag.addEventListener('submit', async event => {
-        event.preventDefault();
-        const formData = new FormData(formTag);
-        const json = JSON.stringify(Object.fromEntries(formData));
-        const locationUrl = 'http://localhost:8000/api/locations/';
-        const fetchConfig = {
-          method: "post",
-          body: json,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-      };
-      const response = await fetch(locationUrl, fetchConfig);
-      if (response.ok) {
-        formTag.reset();
-        const newLocation = await response.json();
-        console.log(newLocation);
-      }
-    });
-  });
+  } catch (e) {
+      console.error("There has been an error")
+  }
+});
